5bfc590a01ad0e642fca48360e59f163d80035a9
Le Phuc <phucng1671@gmail.com>
2023-04-20 17:23:38 +0700

    init

---
+++ activity1_src.R
@@ -0,0 +1,8 @@
+#Bộ dữ liệu "Concrete Compressive Strength": giá trị đo lường độ bền nén
+#của bê tông
+
+# a) Tìm hiểu thông tin về tập dữ liệu
+
+# b) Đọc dữ liệu và đổi tên các cột
+filepath = "C:/Users/DELL/Documents/SP_Assignment/Concrete_Data.xls"
+data<-read.csv(filepath)
