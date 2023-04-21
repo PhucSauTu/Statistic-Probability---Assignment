a3c39e30d98b617756fa1682edf862c9711650a0
Le Phuc <phucng1671@gmail.com>
2023-04-21 18:06:44 +0700

    Summary and Histogram plot added

--- activity1_src.R
+++ activity1_src.R
@@ -1,25 +1,109 @@
 # Packages use:
-library(ggplot2)
+library(ggplot2)  
+library(skimr)
+library(Rmisc)
 
+  #Bộ dữ liệu "Concrete Compressive Strength": giá trị đo lường độ bền nén
+  #của bê tông
 
-#Bộ dữ liệu "Concrete Compressive Strength": giá trị đo lường độ bền nén
-#của bê tông
+  # a) Tìm hiểu thông tin về tập dữ liệu
+  # b) Đọc dữ liệu và đổi tên các cột
 
-# a) Tìm hiểu thông tin về tập dữ liệu
-
-# b) Đọc dữ liệu và đổi tên các cột
-
-    # Luu file xls -> csv
+    # Lưu file xls -> csv
 filepath = "C:/Users/DELL/Documents/SP_Assignment/Concrete_Data.csv"
-    # Doc file
-data<-read.csv(filepath, check.names = F)
+    # Đọc file
+df<-read.csv(filepath, check.names = F)
 
-# c) Tinh trung binh, trung vi, do lech chuan, min, max cho cac bien
+  # c) Tính trung bình, trung vị, đọ lệch chuẩn, min, max cho các biến
+  #Option 1(default):
+summary(df)
+  #Option 2(skimr):
+    #Lý do sử dụng skimr:
+    # + Cung cấp đầy đủ thông tin từ data frame
+    # + So với summary, skimr có thêm standard deviation
+options(pillar.sigfig = 6)  #format 6 số
+skim(df)
 
-summary(data)
+  # d) Vẽ scatter plot và histogram
 
-# d) Ve scatter plot va histogram
+#Histogram
+cementPlot<-ggplot(data = df, mapping = aes(x = `Cement`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 10) +
+  scale_x_continuous(name = paste("Giá trị (",expression(kg/m^3), ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "yellow", high = "red") +
+  ggtitle(substitute(paste(bold("Xi măng"))))
+#show(cementPlot)
 
-plot1 = ggplot(data = data) + geom_boxplot(data = data$cememt)
-View(plot1)
+bfsPlot<-ggplot(data = df, mapping = aes(x = `Blast Furnace Slag`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 10) +
+  scale_x_continuous(name = paste("Giá trị (",expression(kg/m^3), ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "yellow", high = "red") +
+  ggtitle(substitute(paste(bold("Xỉ lò cao"))))
+#show(bfsPlot)
+
+flyashPlot<-ggplot(data = df, mapping = aes(x = `Fly Ash`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 10) +
+  scale_x_continuous(name = paste("Giá trị (",expression(kg/m^3), ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "yellow", high = "red") +
+  ggtitle(substitute(paste(bold("Tro bay"))))
+#show(flyashPlot)
+
+waterPlot<-ggplot(data = df, mapping = aes(x = `Water`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 10) +
+  scale_x_continuous(name = paste("Giá trị (",expression(kg/m^3), ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "cyan", high = "green") +
+  ggtitle(substitute(paste(bold("Nước"))))
+#show(waterPlot)
+
+superplasticizerPlot<-ggplot(data = df, mapping = aes(x = `Superplasticizer`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 5) +
+  scale_x_continuous(name = paste("Giá trị (",expression(kg/m^3), ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "cyan", high = "green") +
+  ggtitle(substitute(paste(bold("Phụ gia siêu dẻo"))))
+#show(superplasticizerPlot)
+
+caPlot<-ggplot(data = df, mapping = aes(x = `Coarse Aggregate`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 10) +
+  scale_x_continuous(name = paste("Giá trị (",expression(kg/m^3), ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "cyan", high = "green") +
+  ggtitle(substitute(paste(bold("Đá"))))
+#show(caPlot)
+
+faPlot<-ggplot(data = df, mapping = aes(x = `Fine Aggregate`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 10) +
+  scale_x_continuous(name = paste("Giá trị (",expression(kg/m^3), ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "purple", high = "cyan") +
+  ggtitle(substitute(paste(bold("Cát"))))
+#show(faPlot)
+
+agePlot<-ggplot(data = df, mapping = aes(x = `Age`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 10) +
+  scale_x_continuous(name = paste("Giá trị (","ngày", ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "purple", high = "cyan") +
+  ggtitle(substitute(paste(bold("Ngày"))))
+#show(agePlot)
+
+ccsPlot<-ggplot(data = df, mapping = aes(x = `Concrete compressive strength`)) +
+  geom_histogram(aes(fill = ..count..), col = "black", bins = 30, binwidth = 5) +
+  scale_x_continuous(name = paste("Giá trị (","MPA", ")", sep = "")) +
+  scale_y_continuous(name = "Tần số") +
+  scale_fill_gradient("Tần số", low = "purple", high = "cyan") +
+  ggtitle(substitute(paste(bold("Cường độ nén bê tông"))))
+#show(ccsPlot)
+
+layout<-matrix(c(1:9), nrow = 3, byrow = TRUE)
+multiplot(cementPlot, bfsPlot, flyashPlot,
+          waterPlot, superplasticizerPlot, caPlot,
+          faPlot, agePlot, ccsPlot
+          ,layout = layout)
+
+#Box Plot & Scatter Plot
 
